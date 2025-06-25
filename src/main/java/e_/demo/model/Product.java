9ee5aeeb8bf108feeee5419172ddce2a9f7d5d09
Achinthya Upadhyaya <Achinthyaupadhyaya@DAC-0162.local>
2025-06-25 23:10:26 +0530

    first commit

---
+++ src/main/java/e_/demo/model/Product.java
@@ -0,0 +1,22 @@
+package e_.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private Double price;
+}
