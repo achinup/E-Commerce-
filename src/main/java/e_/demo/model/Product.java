9e3b8fa968a86b753537b47bd420f5d54e6995d8
Achinthya Upadhyaya <Achinthyaupadhyaya@DAC-0162.local>
2025-07-03 23:15:26 +0530

    lombok added

--- src/main/java/e_/demo/model/Product.java
+++ src/main/java/e_/demo/model/Product.java
@@ -1,9 +1,15 @@
 package e_.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
+@Data // generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor // generates a no-arg constructor
+@AllArgsConstructor // generates an all-args constructor
 public class Product {
     @Id
     private String id;
@@ -11,56 +17,4 @@ public class Product {
     private String description;
     private String category;
     private Double price;
-
-    public Product() {}
-
-    public Product(String id, String name, String description, String category, Double price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.category = category;
-        this.price = price;
-    }
-
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
